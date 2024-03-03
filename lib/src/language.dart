582e076bd690bee6212141aa105fd575cf4a2f05
Arto Bendiken <arto@haltia.ai>
2024-03-03 10:05:26 +0400

    Add more language metadata.

--- lib/src/language.dart
+++ lib/src/language.dart
@@ -1,30 +1,40 @@
 // This is free and unencumbered software released into the public domain.
 
+typedef L = LanguageTag;
+
 /// IETF BCP 47 language tag.
 enum LanguageTag {
   ar("ar", "Arabic", "العربية"),
+  bn("bn", "Bengali", "বাংলা"),
+  de("de", "German", "Deutsch"),
   en("en", "English", "English"),
+  eo("eo", "Esperanto", "Esperanto"),
+  es("es", "Spanish", "español"),
+  et("et", "Estonian", "eesti"),
   fi("fi", "Finnish", "suomi"),
+  fr("fr", "French", "français"),
+  hi("hi", "Hindi", "हिंदी"),
+  id("id", "Indonesian", "Bahasa Indonesia"),
+  it("it", "Italian", "italiano"),
+  ja("ja", "Japanese", "日本語"),
+  ko("ko", "Korean", "한국어"),
+  ms("ms", "Malay", "Bahasa Malaysia"),
+  pl("pl", "Polish", "polski"),
+  pt("pt", "Portuguese", "Português"),
   sv("sv", "Swedish", "svenska"),
-  uk("uk", "Ukrainian", "українська");
-
-  // TODO: add more language tags.
+  tr("tr", "Turkish", "Türkçe"),
+  uk("uk", "Ukrainian", "українська"),
+  ur("ur", "Urdu", "اُردو"),
+  vi("vi", "Vietnamese", "Tiếng Việt"),
+  zh("zh", "Chinese", "中文");
 
   factory LanguageTag.fromString(final String tag) {
-    switch (tag) {
-      case "ar":
-        return ar;
-      case "en":
-        return en;
-      case "fi":
-        return fi;
-      case "sv":
-        return sv;
-      case "uk":
-        return uk;
-      default:
-        throw ArgumentError.value(tag, "tag", "Invalid language tag");
+    for (final lang in LanguageTag.values) {
+      if (lang.tag == tag) {
+        return lang;
+      }
     }
+    throw ArgumentError.value(tag, "tag", "Unknown language tag");
   }
 
   factory LanguageTag.fromJson(final String tag) = LanguageTag.fromString;
